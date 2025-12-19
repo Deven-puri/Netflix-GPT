fcede709f2d319e7bf0162fab1cb72f73d53c1cc
Deven-puri <devenpuri29@gmail.com>
2025-12-19 18:00:00 +0530

    Add constants file and update components to use imported constants

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,15 @@
 import { useNavigate } from "react-router-dom";
-import { signOut } from "firebase/auth";
+import { signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../utils/firebase.tsx";
+import { useEffect } from "react";
+import { removeUser, addUser } from "../utils/userSlice.tsx";
+import { useDispatch } from "react-redux";
+import { LOGO, USER_AVATAR } from "../utils/constants.tsx";
 
 const Header = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    
     const handleSignOut = () => {
         signOut(auth)
             .then(() => {
@@ -13,18 +19,33 @@ const Header = () => {
                 console.error("Sign out error:", error);
             });
     };
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const { uid, email, displayName, photoURL } = user;
+                dispatch(
+                    addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL })
+                );
+                navigate("/browse");
+            } else {
+                dispatch(removeUser());
+            }
+        });
+        // unsubscribe when component unmounts
+        return () => unsubscribe();
+    }, [dispatch, navigate]);
 
     return (
         <div className="fixed top-0 left-0 right-0 w-full px-8 py-4 bg-gradient-to-b from-black via-black/80 to-transparent z-50 flex justify-between items-center">
             <img
                 className="w-32 md:w-40 object-contain"
-                src="https://assets.nflxext.com/en_us/layout/ecweb/common/logo-shadow2x.png"
+                src={LOGO}
                 alt="Netflix logo"
             />
             <div className="flex items-center gap-4">
                 <img 
                     alt="usericon" 
-                    src="https://occ-0-3752-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229" 
+                    src={USER_AVATAR} 
                     className="w-8 h-8 rounded cursor-pointer hover:ring-2 hover:ring-white transition"
                 />
                 <button 
---
+++ src/utils/constants.tsx
@@ -0,0 +1,5 @@
+export const LOGO = "https://assets.nflxext.com/en_us/layout/ecweb/common/logo-shadow2x.png";
+
+export const USER_AVATAR = "https://occ-0-3752-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
+
+export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/29b665f6-0a62-4745-b9c2-f617fb7eadc6/web/IN-en-20251208-TRIFECTA-perspective_c78aea89-8f13-4e2c-ba7a-f9b40f53bf8c_large.jpg";
